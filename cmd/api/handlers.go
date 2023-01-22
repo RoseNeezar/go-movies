c02b0d34cd25680d4f1b96529ba639308d16a3ca
neezar <rforlyffe@gmail.com>
2023-01-22 23:06:04 +0800

    feat: setup movie all route

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -14,3 +14,13 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	}
 	_ = app.writeJSON(w, http.StatusOK, payload)
 }
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.DB.AllMovies()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, movies)
+}
